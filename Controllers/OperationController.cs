78f4b60a1a84ded7b0c8dc46d1d9c316ce76bb8e
Kieran Gelders <k.gelders@gmail.com>
2021-01-02 13:05:05 +1000

    Initial commit

---
+++ Controllers/OperationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleApi.Services;
+
+namespace mini_umb.Controllers
+{
+    public class OperationController : ControllerBase
+    {
+        private readonly IOperationsService _operationsService;
+
+        public OperationController()
+        {
+            _operationsService = new OperationsService();
+        }
+
+        public IActionResult Add(decimal a, decimal b)
+        {
+            return Ok(_operationsService.Add(a, b));
+        }
+    }
+}
